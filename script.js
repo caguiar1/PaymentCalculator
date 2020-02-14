d8fc5e0611c8e62b7e89debde5cf4ba86f02bf58
Christopher Aguiar <epicchris17@gmail.com>
2020-02-14 13:42:36 -0500

    gotta work on only html and css

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 
 
-
-
 function getPrice(){
 
     let price = document.getElementById("price-box").value;
 
-    console.log(price);
+    console.log("Price is: " + price);
 
 
+}
+
+function getDownPayment(){
+
+    let downpayment = document.getElementById("down-payment-box").value;
+
+    console.log("Downpayment is: " + downpayment);
+
+}
+
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
 
 
